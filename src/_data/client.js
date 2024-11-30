eb2763cca3266baa7810fb16dc28e332c2e80730
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2024-11-30 12:48:02 +0000

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "Skipton Thai Massage",
+    "email": "skipton.thaimassage@gmail.com",
+    "phoneForTel": "+447709151651",
+    "phoneFormatted": "07709 151651",
+    "openingHours": "Mon-Sun: 9am-7pm",
+    "onlineBooking": "https://skiptonthaimassage.setmore.com/",
+    "address": {
+        "lineOne": "12 Newmarket St",
+        "lineTwo": "1st Floor",
+        "city": "Skipton",
+        "state": "",
+        "zip": "BD23 2HR",
+        "country": "England",
+        "mapLink": "https://maps.app.goo.gl/PUmwRKhcbBuxAZKW7"
+    },
+    "socials": {
+        "facebook": "",
+        "instagram": "",
+        "googleReview": "https://g.page/r/CTkylKcKiaqREBM/review",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.skiptonthaimassage.co.uk"
+};
