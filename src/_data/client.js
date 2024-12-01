ad139250c43d61a1bd3257e683f95c39c23cd117
andrewbramwell <andy@voucherstore.io>
2024-12-01 14:20:21 +0000

    canocial

--- src/_data/client.js
+++ src/_data/client.js
@@ -22,5 +22,5 @@ module.exports = {
         "googleReview": "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.gorbuathaimassage.co.uk"
+    "domain": "https://gorbuathaimassage.co.uk"
 };
